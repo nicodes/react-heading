622af4e78ab99612934fcd189c532a1d3b71b071
nicodes <nicozessoules@gmail.com>
2023-01-15 08:20:34 -0700

    add type exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,35 @@
 import React, { useRef, useState, useEffect, HTMLAttributes, useCallback } from 'react'
 
 /** Subset of `JSX.InstrinsecElements` for only heading elements */
-type HeadingIntrinsicElements = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+export type HeadingIntrinsicElements = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 
 /** Numeric degrees for headings */
-type HeadingDegrees = 1 | 2 | 3 | 4 | 5 | 6;
+export type HeadingDegrees = 1 | 2 | 3 | 4 | 5 | 6
 
-type Props = { min?: HeadingDegrees, max?: HeadingDegrees } & HTMLAttributes<HTMLHeadingElement>
+/** React heading props */
+export type ReactHeadingProps = { min?: HeadingDegrees, max?: HeadingDegrees } & HTMLAttributes<HTMLHeadingElement>
 
-/** Smart headings for React */
-export default function H({ min = 1, max = 6, children, ...props }: Props): JSX.Element {
+/** Smart Headings for React */
+export default ({ min = 1, max = 6, children, ...props }: ReactHeadingProps): JSX.Element => {
   if (max < min) console.error('`react-heading` invalid props: must meet requirement `min` <= `max`')
+
   const ref = useRef<HTMLHeadingElement>(null)
   const [E, setE] = useState<HeadingIntrinsicElements>(`h${min}`)
 
-  /** Find the number of `<h1>, ..., <h6>` elements in the DOM above by recursing through all parents and grandparent's children "uncles" */
-  const findH = useCallback((parentElement: HTMLElement | null | undefined, n: HeadingDegrees): HeadingDegrees => {
+  /** Returns heading degree by searching for the number of `<h1>, ..., <h6>` elements in the DOM above by recursing through all parents and grandparent's children "uncles" */
+  const headingSearch = useCallback((parentElement: HTMLElement | null | undefined, n: HeadingDegrees): HeadingDegrees => {
     const grandparentElement = parentElement?.parentElement
-    if (!grandparentElement || n === max) return n
+    if (!grandparentElement || n === max)
+      return n
 
     for (const e of Array(grandparentElement.children))
-      if (e[0] instanceof HTMLHeadingElement) return findH(grandparentElement, ++n as HeadingDegrees)
+      if (e[0] instanceof HTMLHeadingElement)
+        return headingSearch(grandparentElement, ++n as HeadingDegrees)
 
-    return findH(grandparentElement, n)
+    return headingSearch(grandparentElement, n)
   }, [max])
 
-  useEffect(() => setE(`h${findH(ref.current?.parentElement, min)}`), [min, max])
+  useEffect(() => setE(`h${headingSearch(ref.current?.parentElement, min)}`), [min, max])
 
   return <E ref={ref} {...props}>{children}</E>
 }
